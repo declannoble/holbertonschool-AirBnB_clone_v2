30a89fbcd9fb97dc855d7b798f9d0e2a7cdbfe4f
Chiara Caprasi <caprasichiara@gmail.com>
2022-08-08 15:09:20 +1000

    fix: error in module initialization

--- models/state.py
+++ models/state.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import String, Column, ForeignKey
+from sqlalchemy import String, Column
 from sqlalchemy.orm import relationship
 import models
 from models.city import City
@@ -10,10 +10,9 @@ from os import getenv
 
 class State(BaseModel, Base):
     """ State class """
-    if models.storage_t == "db":
-        __tablename__ = 'states'
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state")
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state")
 
     if getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
