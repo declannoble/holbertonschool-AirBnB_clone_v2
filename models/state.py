16632fc7b3d33ba61aec95506368d3857c4b4bc9
Chiara Caprasi <caprasichiara@gmail.com>
2022-08-08 15:04:43 +1000

    fix: syntax error on models/states.py

--- models/state.py
+++ models/state.py
@@ -13,10 +13,7 @@ class State(BaseModel, Base):
     if models.storage_t == "db":
         __tablename__ = 'states'
         name = Column(String(128), nullable=False)
-        cities = relationship("City",
-                              backref="state",
-                              cascade='all, delete-orphan'
-                              passive_deletes=True)
+        cities = relationship("City", backref="state")
 
     if getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
