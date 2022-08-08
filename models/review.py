73ff2a3aa87f0e52af0b60a7f27b18aadafe67e7
Chiara Caprasi <caprasichiara@gmail.com>
2022-08-08 18:10:46 +1000

    fix: correct tablename in review.py

--- models/review.py
+++ models/review.py
@@ -4,11 +4,10 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import String, Column, ForeignKey
 
 
-__tablename__ = "reviews"
-
-
 class Review(BaseModel, Base):
     """ Review classto store review information """
+
+    __tablename__ = "reviews"
 
     text = Column(String(1024), nullable=False)
     place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
