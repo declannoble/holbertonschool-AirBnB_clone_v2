deef1bcddad7985e4d94be75f34e9fb0bfe6ffd7
Chiara Caprasi <caprasichiara@gmail.com>
2022-08-08 18:09:06 +1000

    fix: import relationship in city.py

--- models/city.py
+++ models/city.py
@@ -2,6 +2,8 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import String, Column, ForeignKey
+from sqlalchemy.orm import relationship
+
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
