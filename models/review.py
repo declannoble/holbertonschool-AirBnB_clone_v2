74b4fbc4226bc3023538f2bf5e21fb7bdec1ceac
Chiara Caprasi <caprasichiara@gmail.com>
2022-08-08 17:50:23 +1000

    fix: syntax error on ForeignKey

--- models/review.py
+++ models/review.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import String, DateTime, Column, ForeignKey
+from sqlalchemy import String, Column, ForeignKey
 
 
 __tablename__ = "reviews"
@@ -12,5 +11,5 @@ class Review(BaseModel, Base):
     """ Review classto store review information """
 
     text = Column(String(1024), nullable=False)
-    place_id = Column(String(60), ForeignKey=('places.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey=('users.id'), nullable=False)
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
