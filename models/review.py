30b63c2dfa11ef9050a92d094745d65d603e7cc4
Chiara Caprasi <caprasichiara@gmail.com>
2022-08-08 16:59:57 +1000

    build: update review.py as per task 9

--- models/review.py
+++ models/review.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import String, DateTime, Column, ForeignKey
 
 
-class Review(BaseModel):
+__tablename__ = "reviews"
+
+
+class Review(BaseModel, Base):
     """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey=('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey=('users.id'), nullable=False)
