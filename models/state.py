3b55b65d374b70b83a9e2a91341cf8a2fd8e550f
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-08-09 19:39:18 +1000

    build: remove comments in code. Change to include if statement

--- models/state.py
+++ models/state.py
@@ -5,7 +5,7 @@ from sqlalchemy import String, Column
 from sqlalchemy.orm import relationship
 import models
 from models.city import City
-from os import getenv
+# from os import getenv
 
 
 class State(BaseModel, Base):
@@ -14,16 +14,14 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state")
 
-    @property
-    def cities(self):
-        """Getter attribute
-            that returns list of City instances related to state"""
-        list_cities = []
-        all_cities = models.storage.all(City)
-        for city in all_cities.values():
-            if city.state_id == self.id:
-                list_cities.append(city)
-        return list_cities
-
-        # return [city for city in models.storage.all(City).values()
-        #         if city.state_id == self.id]
+    if models.storage_type != "db":
+        @property
+        def cities(self):
+            """Getter attribute
+               that returns list of City instances related to state"""
+            list_cities = []
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    list_cities.append(city)
+            return list_cities
