7c9da06c1c7c01487771b4c5eeabbe2122eb2538
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-08-09 20:56:11 +1000

    fix: only uncomment task 7

--- models/user.py
+++ models/user.py
@@ -2,7 +2,7 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+# from sqlalchemy.orm import relationship
 from models.base_model import BaseModel
 from os import getenv
 
@@ -13,9 +13,8 @@ class User(BaseModel, Base):
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user")
-        reviews = relationship("Review", backref="user")
+        # last_name = Column(String(128), nullable=True)
+        # places = relationship("Place", backref="user")
     else:
         email = ''
         password = ''
