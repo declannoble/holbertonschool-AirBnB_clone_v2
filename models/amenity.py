883b0c8e38177cb865b433dd8301f5f0372f4eac
declannoble <declanjnoble@hotmail.com>
2022-08-09 16:17:45 -0700

    build: add manytomany class attribute

--- models/amenity.py
+++ models/amenity.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-# from sqlalchemy import String, DateTime, Column
-# from sqlalchemy.orm import relationship
+from sqlalchemy import String, DateTime, Column
+from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
-    name = ""
-
-
-# class Amenity(BaseModel, Base):
-
-#     __tablename__ = 'amenities'
-
-#     name = Column(String(128), nullable=False)
+class Amenity(BaseModel, Base):
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary=place_amenity,
+                                   viewonly=False)
