9d0dff2bc12462329ba1349ac0fd0671fb370c1f
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-08-10 21:10:23 +1000

    fix: Code refactor to make cleaner, removed circular import

--- models/amenity.py
+++ models/amenity.py
@@ -2,8 +2,8 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import String, DateTime, Column
-from sqlalchemy.orm import relationship
-from models.place import place_amenity
+# from sqlalchemy.orm import relationship
+# from models.place import place_amenity
 from os import getenv
 
 class Amenity(BaseModel, Base):
@@ -11,7 +11,7 @@ class Amenity(BaseModel, Base):
     if getenv("HBNB_TYPE_STORAGE") == 'db':
         __tablename__ = 'amenities'
         name = Column(String(128), nullable=False)
-        place_amenities = relationship("Place", secondary=place_amenity,
-                                       viewonly=False)
+        # place_amenities = relationship("Place", secondary=place_amenity,
+        #                                viewonly=False)
     else:
         name = ""
--- models/place.py
+++ models/place.py
@@ -9,9 +9,9 @@ from models.review import Review
 from models.amenity import Amenity
 
 
-metadata = Base.metadata
+# metadata = Base.metadata
 if getenv("HBNB_TYPE_STORAGE") == 'db':
-    place_amenity = Table('place_amenity', metadata,
+    place_amenity = Table('place_amenity', Base.metadata,
                         Column('place_id', String(60), ForeignKey("places.id"),
                                 primary_key=True, nullable=False),
                         Column('amenity_id', String(60), ForeignKey(
