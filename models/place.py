72f4673f94738e39e6c3e391b52b82210edcf17e
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-08-10 23:34:19 +1000

    fix: remove quotes around reference.

--- models/place.py
+++ models/place.py
@@ -37,7 +37,7 @@ class Place(BaseModel, Base):
     if getenv("HBNB_TYPE_STORAGE") == 'db':
         reviews = relationship('Review', backref='place',
                                cascade='all, delete-orphan')
-        amenities = relationship('Amenity', secondary='place_amenity',
+        amenities = relationship('Amenity', secondary=place_amenity,
                                  backref="place_amenities",
                                  viewonly=False)
 
