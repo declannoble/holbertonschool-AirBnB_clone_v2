9d21c2aa81da2350b88c036c698b902aff4c096c
declannoble <declanjnoble@hotmail.com>
2022-08-25 09:52:27 +1000

    fix: switch paths for sym link

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -17,7 +17,7 @@ echo "
         </body>
     </html>
 " > /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/current /data/web_static/releases/test/
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current 
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i '/listen 80 default_server;/a location /hbnb_static {alias /data/web_static/current; autoindex off;}' /etc/nginx/sites-available/default
 sudo service nginx restart
