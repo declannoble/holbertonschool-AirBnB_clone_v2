25a9cbe5462c286120b8f8cf212f47a884fb8971
declannoble <declanjnoble@hotmail.com>
2022-08-26 15:10:02 +1000

    fix: redo all previous tasks

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -19,5 +19,5 @@ echo "
 " > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '/listen 80 default_server;/a location /hbnb_static { alias /data/web_static/current/; autoindex off }' /etc/nginx/sites-available/default
+sed -i '/listen 80 default_server;/a location /hbnb_static { \n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 sudo service nginx restart
